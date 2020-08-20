fa61b87085f1eb4fdce23cb4238653f2b49e0ad5
Brandon Weaver <a7xncob@gmail.com>
2020-08-20 16:58:12 -0400

    Add loops

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 message = "Python"
 print(message)
 
-# Interpolation
+# String formatting
 name = "Brandon Weaver"
 print("My name is {}.".format(name))
 
@@ -12,3 +12,20 @@ if number > 10:
     print("{} is greater than 10.".format(number))
 else:
     print("{} is not greater than 10.".format(number))
+
+# Loops
+people = ["Jane", "Joe", "John"]
+
+print("\nFor iterator in range:")
+for i in range(len(people)):
+    print(people[i])
+
+print("\nFor element in array:")
+for person in people:
+    print(person)
+
+print("\nWhile:")
+i = 0
+while i < len(people):
+    print(people[i])
+    i += 1
