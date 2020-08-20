9e19e7609b57a6248526f8c613a26d76522fefe0
Brandon Weaver <a7xncob@gmail.com>
2020-08-20 16:47:10 -0400

    Add string formatting and conditionals

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 message = "Python"
 print(message)
+
+# Interpolation
+name = "Brandon Weaver"
+print("My name is {}.".format(name))
+
+# Conditionals
+number = 5
+
+if number > 10:
+    print("{} is greater than 10.".format(number))
+else:
+    print("{} is not greater than 10.".format(number))
